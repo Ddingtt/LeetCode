c8aa51696c6d2d08ec97cbaf6ff55dfede0e228d
丁浩天 <839338977@qq.com>
2019-11-19 18:36:07 +0100

    2019/11/19
    ！！！！！！！！！！！！
    没做出来

---
+++ Top100/124.binary-tree-maximum-path-sum.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=124 lang=java
+ *
+ * [124] Binary Tree Maximum Path Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    //！！！！！！！！！！！！！！！！！！！
+    int maxValue;
+    
+    public int maxPathSum(TreeNode root) {
+        maxValue = Integer.MIN_VALUE;
+        maxPathDown(root);
+        return maxValue;
+    }
+    
+    private int maxPathDown(TreeNode node) {
+        if (node == null) return 0;
+        int left = Math.max(0, maxPathDown(node.left));
+        int right = Math.max(0, maxPathDown(node.right));
+        maxValue = Math.max(maxValue, left + right + node.val);
+        return Math.max(left, right) + node.val;
+    }
+}
+// @lc code=end
+
