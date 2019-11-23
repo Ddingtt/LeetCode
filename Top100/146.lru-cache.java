7338b91089838cdaf53428a01bcbdbe9c99d2501
丁浩天 <839338977@qq.com>
2019-11-23 11:39:07 +0100

    2019/11/23
    ！！！！！
    不会
    hashmap

---
+++ Top100/146.lru-cache.java
@@ -0,0 +1,50 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode id=146 lang=java
+ *
+ * [146] LRU Cache
+ */
+
+// @lc code=start
+class LRUCache {
+    private Map<Integer, Integer> map;
+
+    public LRUCache(int capacity) {
+        map = new LinkedCappedHashMap(capacity);
+    }
+    
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        return map.get(key);
+    }
+    
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+
+    private static class LinkedCappedHashMap<K,V> extends LinkedHashMap<K,V>{
+        int maximumCapacity;
+
+        LinkedCappedHashMap(int maximumCapacity){
+            super(16, 0.75f, true);
+            this.maximumCapacity = maximumCapacity;
+        }
+
+        protected boolean removeEldestEntry(Map.Entry eldest){
+            return size() > maximumCapacity;
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+// @lc code=end
+
