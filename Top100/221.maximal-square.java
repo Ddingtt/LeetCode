7e493e0797a39efe87abc44e620e27586c6341ed
丁浩天 <839338977@qq.com>
2019-12-03 23:55:33 +0100

    2019/12/03

---
+++ Top100/221.maximal-square.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=221 lang=java
+ *
+ * [221] Maximal Square
+ */
+
+// @lc code=start
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if(matrix.length == 0) return 0;
+        int m = matrix.length, n = matrix[0].length, result = 0;
+        int[][] b = new int[m+1][n+1];
+        for (int i = 1 ; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(matrix[i-1][j-1] == '1') {
+                    //矩阵一个位置为1，看他前侧上侧前上侧的数，
+                    //都为1，则正方形边长扩大1（min（1，1，1） + 1 = 2）
+                    //一旦有一个0，无法扩大   （min（0，0，x） + 1 = 1）
+                    b[i][j] = Math.min(Math.min(b[i][j-1] , b[i-1][j-1]), b[i-1][j]) + 1;
+                    result = Math.max(b[i][j], result); // update result
+                }
+            }
+        }
+        return result*result;
+    }
+}
+// @lc code=end
+
