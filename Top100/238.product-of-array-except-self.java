473244e6447bde0bb8674d390b5f93dfa27b7c4a
丁浩天 <839338977@qq.com>
2019-12-05 19:02:07 +0100

    2019/12/05

---
+++ Top100/238.product-of-array-except-self.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=238 lang=java
+ *
+ * [238] Product of Array Except Self
+ */
+
+// @lc code=start
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        //eg[2,3,4,5]
+        int n = nums.length;
+        int[] res = new int[n];
+        // Calculate lefts and store in res.
+        int left = 1;
+        for (int i = 0; i < n; i++) {
+            if (i > 0)
+                left = left * nums[i - 1];
+            res[i] = left;
+            //[1,2,6,24]
+        }
+        // Calculate rights and the product from the end of the array.
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i < n - 1)
+                right = right * nums[i + 1];
+            res[i] *= right;
+            //[3*4*5,4*5,5,1]
+            //[60,40,30,24]
+        }
+        return res;
+    }
+}
+// @lc code=end
+
