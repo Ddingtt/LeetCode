276b294753bec9516440ee328a41e49a6e5946a9
丁浩天 <839338977@qq.com>
2019-12-11 22:30:51 +0100

    2019/12/11

---
+++ Top100/283.move-zeroes.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=283 lang=java
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+
+        int insertPos = 0;
+        for (int num: nums) {
+            if (num != 0) {
+                nums[insertPos] = num;
+                insertPos++;
+            }
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos] = 0;
+            insertPos++;
+        }
+    }
+}
+// @lc code=end
+
